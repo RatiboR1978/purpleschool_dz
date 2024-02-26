23bbf2f19e7ee786b009a34ced7139a2e122b487
Ratibor <klov29@mail.ru>
2024-02-26 17:25:11 +0700

    4-navigator

---
+++ 4-navigator/navigator.js
@@ -0,0 +1,8 @@
+/*-- 4.11. Домашнее задание - Основы --*/
+
+const addresLat = 10;
+const addresLong = 10;
+const positionLat = 1;
+const positionLong = 1;
+
+const spacingPoint = Math.sqrt((addresLat - positionLat)**2 + (addresLong - positionLong)**2);
