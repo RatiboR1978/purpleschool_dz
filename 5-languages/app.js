4556126ba65f6c0bab43ad57f27341b8dfeba5fa
Ratibor <klov29@mail.ru>
2024-02-27 10:21:03 +0700

    5-languages

---
+++ 5-languages/app.js
@@ -0,0 +1,15 @@
+const language = 'en';
+
+switch(language) {
+	case 'en':
+		console.log('Hello');
+		break;
+	case 'ru':
+		console.log('Привет');
+		break;
+	case 'de':
+		console.log('Guten Tag');
+		break;
+	default:
+		console.log('Не знаю такого языка');
+}
