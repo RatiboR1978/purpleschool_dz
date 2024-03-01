2c0b2b3506333b01993d1c5474d4f60833951ae7
Ratibor <klov29@mail.ru>
2024-03-01 12:10:06 +0700

    code review 1

---
+++ 6-driver/script.js
@@ -0,0 +1,7 @@
+/*-- 6.7. Домашнее задание - Булевая логика --*/
+
+const age = 19;
+const hasLicence = true;
+const isDrink = false;
+
+console.log((age > 18 && hasLicence && !isDrink) ? 'Может вести машину' : 'Не может вести машину');
